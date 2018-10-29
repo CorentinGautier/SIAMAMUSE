800b51112806d684e7fb18a9f864ebbcccd7154c
CorentinGautier <43545800+CorentinGautier@users.noreply.github.com>
2018-10-29 17:33:45 +0100

    f

--- app/src/main/java/corp/siam/siamamuse/option_jeu.java
+++ app/src/main/java/corp/siam/siamamuse/option_jeu.java
@@ -11,6 +11,7 @@ public class option_jeu extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_option_jeu);
     }
+
     public void RetourMenu(View view) { //quitter l'activity actuel.
         this.finish();
     }
