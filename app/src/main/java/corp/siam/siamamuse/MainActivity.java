fdb3b5cb5da7d6d4200464863f5e497c59b37ad1
CorentinGautier <43545800+CorentinGautier@users.noreply.github.com>
2018-10-29 16:07:15 +0100

    hello

--- app/src/main/java/corp/siam/siamamuse/MainActivity.java
+++ app/src/main/java/corp/siam/siamamuse/MainActivity.java
@@ -1,13 +1,31 @@
 package corp.siam.siamamuse;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Spinner;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity { //page d'acceuil
 
+    Spinner spinner;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void ActivityOption(View v){
+        Intent intent = new Intent(this, option.class); // l'activité où on est en ce moment et la prochaine activity
+        startActivity(intent);
+    }
+
+    public void ActivityJeu(View v){
+        Intent intent = new Intent(this, option_jeu.class); // l'activité où on est en ce moment et la prochaine activity
+        startActivity(intent);
+    }
+
+    public void QuitterLeJeu(View view) { //quitter l'activity actuel.
+        this.finish();
     }
 }
