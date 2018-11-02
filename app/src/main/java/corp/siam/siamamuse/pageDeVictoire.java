2e552af5544603d706c94fcc35a954d3b6d82ace
CorentinGautier <43545800+CorentinGautier@users.noreply.github.com>
2018-11-02 12:31:34 +0100

    hello

--- app/src/main/java/corp/siam/siamamuse/pageDeVictoire.java
+++ app/src/main/java/corp/siam/siamamuse/pageDeVictoire.java
@@ -12,10 +12,8 @@ public class pageDeVictoire extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_page_de_victoire);
     }
-    public void RetourMenu(View view) { //quitter l'activity actuel.
-        this.finish();
-    }
-    public void LancerLeJeux(View v){
+
+    public void QuitterPageDeVictoire(View v){
         Intent intent = new Intent(this, MainActivity.class); // l'activité où on est en ce moment et la prochaine activity
         startActivity(intent);
     }
