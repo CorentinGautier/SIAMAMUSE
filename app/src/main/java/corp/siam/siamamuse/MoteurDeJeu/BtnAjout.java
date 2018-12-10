14f7dbf054bc6b8d91f00447dd464ad878623204
Glenn <glenn.louarn@gmail.com>
2018-12-10 13:39:05 +0100

    ajout fonction ajouterPion sur le plateau fonctionnel

--- app/src/main/java/corp/siam/siamamuse/MoteurDeJeu/BtnAjout.java
+++ app/src/main/java/corp/siam/siamamuse/MoteurDeJeu/BtnAjout.java
@@ -1,9 +1,8 @@
 package corp.siam.siamamuse.MoteurDeJeu;
 
-import android.util.Log;
+
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.ImageButton;
 
 import corp.siam.siamamuse.Activity_Partie;
