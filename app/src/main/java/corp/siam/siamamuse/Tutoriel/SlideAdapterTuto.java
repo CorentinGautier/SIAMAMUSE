d8b6df297d2fc2694a66637ddf701f281d39adc9
CorentinGautier <43545800+CorentinGautier@users.noreply.github.com>
2019-01-13 20:51:25 +0100

    beau tuto

--- app/src/main/java/corp/siam/siamamuse/Tutoriel/SlideAdapterTuto.java
+++ app/src/main/java/corp/siam/siamamuse/Tutoriel/SlideAdapterTuto.java
@@ -29,19 +29,16 @@ public class SlideAdapterTuto extends PagerAdapter {
             R.drawable.deplaceemntous,
             R.drawable.caserouge,
             R.drawable.timer,
-
-
-
     };
 
     // liste de titre
     public String[] lst_title = {
             "Bienvenue sur le tutoriel",
             "Ajout d'un pion",
-            "Case verte",
-            "Positionnement d'un pion",
+            "Cases vertes",
+            "Flèches rouges",
             "Choix des déplacements possibles",
-            "Zones rouges",
+            "Indication au long de la partie",
             "Timer",
 
 
@@ -49,22 +46,22 @@ public class SlideAdapterTuto extends PagerAdapter {
 
     // liste de descriptions
     public String[] lst_description = {
-            "Swiper vers le droite ou la gauche pour voir le tutoriel",
-            "Pour pouvoir ajouter un pion, on doit tout d’abord cliquer sur le pion que l’on veut placer dans la liste de pion sur l’écran.",
-            "Ensuite alors apparaît sur le plateau des cases vertes où l'on vous indique dans quel endroit placer votre pion",
-            "Vous devez maiantenant choisir l’orientation de votre pion pour finaliser l’action",
-            "Vous pouvez faire un double tap ou alors choisir le direction en cliquant sur les flèches rouges lorsque vous avez soit cliquer sur le bouton rotation ou alors fait le deplacement du pion choisit",
-            "Après que chacun des joueurs aient ajoutés un pion dans le plateau on obtient ce plateau par exemple. Lorsque l’on veut déplacer un pion du plateau",
-            "Lorsque le timer est à zéro on passe son tour au joueur suivant. Il peut être désactivé dans les options de début de partie comme dit plus haut.",
+            "Swiper vers la droite ou la gauche pour voir le tutoriel",
+            "Pour pouvoir ajouter un pion, on doit tout d’abord cliquer sur le pion dans la liste de votre côter de l'écran que l’on souhaite placer sur l’écran.",
+            "Lors de la selection d'un pion, des cases vertes apparaissent et nous propose des cases où l'on peut endroit placer notre pion",
+            "Les flèches rouges indique l’orientation de votre pion pour finaliser l’action",
+            "Vous pouvez faire un double tap ou alors choisir le direction en cliquant sur les flèches rouges lorsque vous avez cliquer sur le bouton rotation ou choisit sur les cases vertes la position du pion choisit",
+            "Après que chacun des joueurs aient ajoutés des pion sur le plateau, on obtient ce plateau par exemple. Lorsque l’on veut déplacer un pion du plateau, il est indiqué vos choix possibles de jeu",
+            "Lorsque le timer est à zéro on passe son tour au joueur suivant. Il peut être désactivé ou modifié dans les options de début de partie",
     };
 
     // list des couleurs en background
     public int[] lst_backgroundcolor = {
-            Color.rgb(28, 100, 30),
-            Color.rgb(55, 55, 55),
+            Color.rgb(74, 255, 51),
+            Color.rgb(51, 255, 251),
             Color.rgb(239, 85, 85),
             Color.rgb(110, 49, 89),
-            Color.rgb(200, 42, 1),
+            Color.rgb(255, 215, 51),
             Color.rgb(4, 42, 167),
             Color.rgb(150, 4, 200),
 
