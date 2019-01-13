6ff65cb17904303d5dd7f0ffe8142e7957aa6871
Glenn <glenn.louarn@gmail.com>
2019-01-13 16:58:21 +0100

    Redimensionnement btn page menu

--- app/src/main/java/corp/siam/siamamuse/MainActivity.java
+++ app/src/main/java/corp/siam/siamamuse/MainActivity.java
@@ -1,32 +1,64 @@
 package corp.siam.siamamuse;
 
-import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.view.View;
-import android.widget.ArrayAdapter;
+import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.ListView;
-import android.widget.Spinner;
-
-import java.util.ArrayList;
 
 import corp.siam.siamamuse.Plateau.Activity_CreationPlateau;
-import corp.siam.siamamuse.Tutoriel.Activity_Tutoriel;
 
 public class MainActivity extends AppCompatActivity { //page d'acceuil
+
+    public int largeurEcrant,hauteurEcrant;
+    private Button btnJouer;
+    private Button btnQuitter;
+    private Button btnEditerPlateau;
+    private Button btnTutoriel;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         verificationBDDRemplie();
-
+        btnEditerPlateau = (Button) findViewById(R.id.buttonEditplateau);
+        btnJouer = (Button) findViewById(R.id.buttonJouer);
+        btnQuitter = (Button) findViewById(R.id.buttonQuitter);
+        calculTailleEcrant();
+        dispositionInterface();
     }
 
+    public void dispositionInterface(){
+        //coordX
+        btnJouer.setX((int)(largeurEcrant*0.35));
+        btnEditerPlateau.setX((int)(largeurEcrant*0.35));
+        btnQuitter.setX((int)(largeurEcrant*0.35));
+        //CoordY
+        btnJouer.setY((int)(hauteurEcrant*0.40));
+        btnEditerPlateau.setY((int)(hauteurEcrant*0.55));
+        btnQuitter.setY((int)(hauteurEcrant*0.70));
+
+        //Redimmensionnement
+        ViewGroup.LayoutParams paramsButtonJouer = btnJouer.getLayoutParams();
+        paramsButtonJouer.width = (int)(largeurEcrant*0.3);
+        paramsButtonJouer.height = (int)(hauteurEcrant*0.1);
+        ViewGroup.LayoutParams paramsButtonQuitter = btnQuitter.getLayoutParams();
+        paramsButtonQuitter.width = (int)(largeurEcrant*0.3);
+        paramsButtonQuitter.height = (int)(hauteurEcrant*0.1);
+        ViewGroup.LayoutParams paramsButtonEditer = btnEditerPlateau.getLayoutParams();
+        paramsButtonEditer.width = (int)(largeurEcrant*0.3);
+        paramsButtonEditer.height = (int)(hauteurEcrant*0.1);
+    }
+
+    public void calculTailleEcrant(){
+        DisplayMetrics metrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(metrics);
+        largeurEcrant = metrics.widthPixels;
+        hauteurEcrant = metrics.heightPixels;
+    }
 
     public void allerEditPlateau(View v){
         Intent intent = new Intent(this, Activity_CreationPlateau.class); // l'activité où on est vers la prochaine
